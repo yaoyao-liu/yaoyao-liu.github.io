1167f6cccdbc78a821fac39f7fe98e1fa9c1b17a
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2022-12-28 17:32:16 -0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -29,3 +29,35 @@ shieldio_data_mtl = {
 }
 with open(f'results/gs_data_shieldsio_mtl.json', 'w') as outfile:
     json.dump(shieldio_data_mtl, outfile, ensure_ascii=False)
+    
+shieldio_data_mnemonics = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:k_IJM867U9cC']['num_citations']}",
+}
+with open(f'results/gs_data_shieldsio_mnemonics.json', 'w') as outfile:
+    json.dump(shieldio_data_mnemonics, outfile, ensure_ascii=False)
+    
+shieldio_data_aanets = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:u_35RYKgDlwC']['num_citations']}",
+}
+with open(f'results/gs_data_shieldsio_aanets.json', 'w') as outfile:
+    json.dump(shieldio_data_aanets, outfile, ensure_ascii=False)
+    
+shieldio_data_e3bm = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:vV6vV6tmYwMC']['num_citations']}",
+}
+with open(f'results/gs_data_shieldsio_e3bm.json', 'w') as outfile:
+    json.dump(shieldio_data_e3bm, outfile, ensure_ascii=False)
+   
+shieldio_data_lst = {
+  "schemaVersion": 1,
+  "label": "citations",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:TFP_iSt0sucC']['num_citations']}",
+}
+with open(f'results/gs_data_shieldsio_lst.json', 'w') as outfile:
+    json.dump(shieldio_data_lst, outfile, ensure_ascii=False)
