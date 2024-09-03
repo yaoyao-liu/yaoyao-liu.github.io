869fa9fcda4a9d706ec11fc1d52c7528d5bbab10
Yaoyao Liu <yaoyaoliu@outlook.com>
2024-09-03 12:55:15 -0500

    Update google_scholar_crawler

--- google_scholar_crawler/backup.py
+++ google_scholar_crawler/backup.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime
 import os
 
-author: dict = scholarly.search_author_id('Qi2PSmEAAAAJ')
+author: dict = scholarly.search_author_id('Uf9GqRsAAAAJ')
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
@@ -25,7 +25,7 @@ with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
 shieldio_data_mtl = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['publications']['Qi2PSmEAAAAJ:Tyk-4Ss8FVUC']['num_citations']}",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:bEWYMUwI8FkC']['num_citations']}",
 }
 with open(f'results/gs_data_shieldsio_mtl.json', 'w') as outfile:
     json.dump(shieldio_data_mtl, outfile, ensure_ascii=False)
@@ -33,7 +33,7 @@ with open(f'results/gs_data_shieldsio_mtl.json', 'w') as outfile:
 shieldio_data_mnemonics = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['publications']['Qi2PSmEAAAAJ:UeHWp8X0CEIC']['num_citations']}",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:k_IJM867U9cC']['num_citations']}",
 }
 with open(f'results/gs_data_shieldsio_mnemonics.json', 'w') as outfile:
     json.dump(shieldio_data_mnemonics, outfile, ensure_ascii=False)
@@ -41,7 +41,7 @@ with open(f'results/gs_data_shieldsio_mnemonics.json', 'w') as outfile:
 shieldio_data_aanets = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['publications']['Qi2PSmEAAAAJ:u-x6o8ySG0sC']['num_citations']}",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:u_35RYKgDlwC']['num_citations']}",
 }
 with open(f'results/gs_data_shieldsio_aanets.json', 'w') as outfile:
     json.dump(shieldio_data_aanets, outfile, ensure_ascii=False)
@@ -49,7 +49,7 @@ with open(f'results/gs_data_shieldsio_aanets.json', 'w') as outfile:
 shieldio_data_e3bm = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['publications']['Qi2PSmEAAAAJ:d1gkVwhDpl0C']['num_citations']}",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:vV6vV6tmYwMC']['num_citations']}",
 }
 with open(f'results/gs_data_shieldsio_e3bm.json', 'w') as outfile:
     json.dump(shieldio_data_e3bm, outfile, ensure_ascii=False)
@@ -57,7 +57,7 @@ with open(f'results/gs_data_shieldsio_e3bm.json', 'w') as outfile:
 shieldio_data_lst = {
   "schemaVersion": 1,
   "label": "citations",
-  "message": f"{author['publications']['Qi2PSmEAAAAJ:2osOgNQ5qMEC']['num_citations']}",
+  "message": f"{author['publications']['Uf9GqRsAAAAJ:TFP_iSt0sucC']['num_citations']}",
 }
 with open(f'results/gs_data_shieldsio_lst.json', 'w') as outfile:
     json.dump(shieldio_data_lst, outfile, ensure_ascii=False)
